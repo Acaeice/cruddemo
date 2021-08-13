84b7f0df40f56212221cde7e3c3be6dcd140be26
“wuzuo” <wanghejun@meikeland.com>
2021-08-13 18:33:39 +0800

    fix

--- api/cruddemo/middleware.go
+++ api/cruddemo/middleware.go
@@ -2,6 +2,7 @@ package cruddemo
 
 import (
 	"log"
+	"net/http"
 	"strconv"
 	"strings"
 
@@ -97,4 +98,23 @@ func logUserVisit(c *gin.Context) {
 	// }()
 
 	c.Next()
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		// 处理请求
+		c.Next()
+	}
 }
