135e0d379ee7b2484febcbb919d6a71cde9e2898
“wuzuo” <wanghejun@meikeland.com>
2021-08-15 20:10:59 +0800

    fix

--- api/cruddemo/middleware.go
+++ api/cruddemo/middleware.go
@@ -1,8 +1,8 @@
 package cruddemo
 
 import (
+	"fmt"
 	"log"
-	"net/http"
 	"strconv"
 	"strings"
 
@@ -100,21 +100,26 @@ func logUserVisit(c *gin.Context) {
 	c.Next()
 }
 
-func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
-
-		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
+func CORS() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// 允许 Origin 字段中的域发送请求
+		context.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		// 设置预验请求有效期为 86400 秒
+		context.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		// 设置允许请求的方法
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
+		// 设置允许请求的 Header
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, token")
+		// 设置拿到除基本字段外的其他字段，如上面的Apitoken, 这里通过引用Access-Control-Expose-Headers，进行配置，效果是一样的。
+		context.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Headers")
+		// 配置是否可以带认证信息
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		// OPTIONS请求返回200
+		if context.Request.Method == "OPTIONS" {
+			fmt.Println(context.Request.Header)
+			context.AbortWithStatus(200)
+		} else {
+			context.Next()
 		}
-		// 处理请求
-		c.Next()
 	}
 }
